8dc401a94c8fca716d4d5dfc6eb67d37e213b6ff
Javier Alonso <notjavo7@gmail.com>
2024-06-14 15:06:22 -0400

    hi

--- problem1.py
+++ problem1.py
@@ -1,19 +1,21 @@
-def sum_of_multiples(limit):
-    #initalize the sum
-    total_sum=0
+def calculate_sum(limit):
+    # Initialize the sum
+    total_sum = 0
 
-    #loop through each number from 1 to limit -1
+    # Loop through each number from 1 to limit - 1
     for number in range(limit):
-        #belowing checking if the number is a multiple of three and five
+        # Check if the number is a multiple of 3 or 5
         if number % 3 == 0 or number % 5 == 0:
-            #next, adding the number to total sum
+            # Add the number to total sum
             total_sum += number
 
-        return total_sum
-    
-    #next I will define the limit
-    limit=1000
+    print(f"The sum of all multiples of 3 and 5 below {limit} is {total_sum}")
 
-    #calling the funtion and printing the result
-    result = sum_of_multiples
-    print(f"The sum of all multiples of 3 and 5 below {limit} is {result}")
+# Define the limit
+limit = 1000
+
+# Call the function with the limit
+calculate_sum(limit)
+
+
+
